d9946151f13fe28850d971ec1a903c13efa527d8
Kramptj KC <kramptj@gmail.com>
2024-10-30 02:35:58 -0400

    Created Firebase DB Connection & Edited Gradle Files
    Created a Firebase DB through the browser console. Went through
    the steps to initialize the DB by creating the google-services.json
    file and editing the gradle files to include the google-services
    json file. Edited MainActivity.java to test out connection to the
    firebase DB and it came out successful.

--- app/src/main/java/com/example/localfinder_app/MainActivity.java
+++ app/src/main/java/com/example/localfinder_app/MainActivity.java
@@ -2,23 +2,34 @@ package com.example.localfinder_app;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.util.Log; // Allows to use the Log class to send log messages to Logcat
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase; // Importing Firebase DB
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Initialize Firebase Realtime Database
+        FirebaseDatabase firebaseDB = FirebaseDatabase.getInstance();
+        DatabaseReference dbRef = firebaseDB.getReference("testNode");
+
+        // Write a message to the DB
+        dbRef.setValue("Firebase setup successful!") // The output to the Firebase console
+                .addOnCompleteListener( task -> {
+
+                    // Output to Logcat terminal
+                    // Tag is the tag name that appears in Logcat
+                    // Msg is the message attached to the Tag name.
+                    if (task.isSuccessful()) {
+                        Log.d("FirebaseTest", "Data written successfully"); // Debug message
+                    } else {
+                        Log.e("FirebaseTest", "Failed to write data", task.getException()); // Error message
+                    }
+                });
     }
 }
