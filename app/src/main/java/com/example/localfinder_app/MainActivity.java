0838cb8f45c7d3a5ed2fee8a744d6015e5f17d7d
Kramptj KC <kramptj@gmail.com>
2024-10-30 23:11:09 -0400

    Added Query functionality, Created .gitattributes
    Created a .gitattributes file to convert line ending to type CRLF
    since this project will be worked on in a windows device until
    submission of the assignment. MainActivity.java now has a new
    functionality where it can query the FirebaseDB to output
    longitude and latitude values for a given address that must exist
    inside the FirebaseDB.

--- app/src/main/java/com/example/localfinder_app/MainActivity.java
+++ app/src/main/java/com/example/localfinder_app/MainActivity.java
@@ -4,8 +4,13 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log; // Allows to use the Log class to send log messages to Logcat
+import androidx.annotation.NonNull;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase; // Importing Firebase DB
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.DatabaseError;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,7 +22,34 @@ public class MainActivity extends AppCompatActivity {
         // Calling the setup method to initialize data in Firebase DB
         // Should be commented out after running it the first time
         // setupInitialDB();
+
+        // Placeholder value for user-input address input. This value will come from the UI later on
+        String userAddressInput = "123 Main St, Toronto, ON";
+
+        // Call queryLocationByAddress method with the sample address and handle result using callback
+        queryLocationByAddress(userAddressInput, new QueryResultCallback() {
+            @Override
+
+            // Method to handle the query results once they are retrieved from FirebaseDB
+            // This method allows the queryLocationByAddress method to notify once the query is complete
+            public void queryResult(Location locationReceivedFromFirebase) {
+
+                // Check to see if location was found from FirebaseDB
+                if (locationReceivedFromFirebase != null) {
+
+                    // Log the latitude and longitude of the found location into Logcat
+                    Log.d("QueryResult", "Latitude: " + locationReceivedFromFirebase.latitudeValue);
+                    Log.d("QueryResult", "Longitude: " + locationReceivedFromFirebase.longitudeValue);
+
+                } else {
+
+                    // Log message when no matching address was found
+                    Log.d("QueryErrorResult", "No matching address found.");
+                }
+            }
+        });
     }
+
 
     // Method to set up initial location data in FirebaseDB
     private void setupInitialDB() {
@@ -60,6 +92,68 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
     }
+
+
+    // Method to query FirebaseDB by user-input address and result results from DB through a callback
+    public void queryLocationByAddress(String userAddressInput, QueryResultCallback callback) {
+
+        // Reference to the "Locations" node in FirebaseDB
+        DatabaseReference locationsDBRef = FirebaseDatabase.getInstance().getReference("Locations");
+
+        // Create a query that searches for the given address
+        Query query = locationsDBRef.orderByChild("address").equalTo(userAddressInput);
+
+        // Listener to retrieve data from FirebaseDB
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                // Check to see if any data was returned
+                if (dataSnapshot.exists()) {
+
+                    // Array creation to hold snapshots of matching results
+                    DataSnapshot[] locationSnapshotsArray = new DataSnapshot[(int) dataSnapshot.getChildrenCount()];
+                    int snapshotIndex = 0;
+
+                    // Convert dataSnapshot's children to an array
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        locationSnapshotsArray[snapshotIndex++] = snapshot;
+                    }
+
+                    // For loop to process each snapshot in the array
+                    Location locationQueryResult = null;
+                    for (int arrayIndex = 0; arrayIndex < locationSnapshotsArray.length; arrayIndex++) {
+                        locationQueryResult = locationSnapshotsArray[arrayIndex].getValue(Location.class);
+
+                        // Breaks after retrieving the first valid location
+                        if (locationQueryResult != null)
+                            break;
+                    }
+
+                    // Pass the found location to the callback
+                    callback.queryResult(locationQueryResult);
+
+                } else {
+                    callback.queryResult(null);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                // Log an error if query fails
+                Log.e("QueryResult", "Database query failed", databaseError.toException());
+
+                callback.queryResult(null);
+            }
+        });
+    }
+
+    // Callback interface to handle query results
+    public interface QueryResultCallback {
+        void queryResult(Location locationReceivedFromFirebase);
+    }
+
 
     // Location class to represent each location entry in Firebase DB
     public static class Location {
